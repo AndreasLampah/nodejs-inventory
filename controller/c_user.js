071726e6e45891ca0da7c777d5553d078b2807f5
AndreasLampah <andreaslampah97@gmail.com>
2024-07-02 19:31:17 +0800

    table master kategori, master produk, stok

--- controller/c_user.js
+++ controller/c_user.js
@@ -5,6 +5,7 @@ module.exports =
     index: async function(req,res) {
         let dataview = {
             konten: 'user-management/index',
+            req: req,
             uri_segment: req.path.split('/'),
             users: await m_user.get_semua_user()
         }
@@ -29,10 +30,16 @@ module.exports =
             let insert      = await m_user.tambah(req)
             let isi_notif   = `berhasil membuat user baru`
             if (insert.affectedRows > 0) {
-                res.redirect(`/user-management?note=${isi_notif}`)
+                res.redirect(`/user-management?note=sukses&pesan=${isi_notif}`)
             }
         } catch (error) {
-            res.render('/user/tambah', {info_error: error})
+            let dataview = {
+                konten      : 'user-management/form-tambah',
+                req         : req,
+                uri_segment : req.path.split('/'),
+                info_error  : error,
+            }
+            res.render('template/struktur', dataview)
         }
     },
 
