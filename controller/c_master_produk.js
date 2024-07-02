513fa1738f869f2245eed4706a15e4bd418d38bc
AndreasLampah <andreaslampah97@gmail.com>
2024-07-02 21:59:14 +0800

    .\.git\

--- controller/c_master_produk.js
+++ controller/c_master_produk.js
@@ -1,9 +1,25 @@
+const m_produk = require('../model/m_produk')
+
 module.exports =
 {
-    index: function(req,res) {
+    index: async function(req,res) {
         let dataview = {
-            konten: 'master-produk/index',
-            uri_segment: req.path.split('/'),
+            req         : req,
+            konten      : 'master-produk/index',
+            uri_segment : req.path.split('/'),
+            produk      : await m_produk.get_semua_produk()
+        }
+        res.render('template/struktur', dataview)
+    },
+
+
+
+    form_tambah: async function(req,res) {
+        let dataview = {
+            konten      : 'master-produk/form-tambah',
+            uri_segment : req.path.split('/'),
+            info_error  : null,
+            kategori    : await m_produk.get_semua_kategori()
         }
         res.render('template/struktur', dataview)
     },
--- model/m_produk.js
+++ model/m_produk.js
@@ -1,11 +1,5 @@
-const mysql     = require('mysql2')
-const db        = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'xdb_belajar_database',
-})
-db.connect()
+const mysql = require('mysql2')
+const db    = require('../config/database').db
 
 
 module.exports =
@@ -13,6 +7,24 @@ module.exports =
     get_semua_produk: function() {
         let sql = mysql.format(
             `SELECT * FROM master_produk;`
+        )
+
+        return new Promise( (resolve,reject)=>{
+            db.query(sql, function(errorSql, hasil) {
+                if (errorSql) {
+                    reject(errorSql)
+                } else {
+                    resolve(hasil)
+                }
+            })
+        })
+    },
+
+
+
+    get_semua_kategori: function() {
+        let sql = mysql.format(
+            `SELECT * FROM master_kategori;`
         )
 
         return new Promise( (resolve,reject)=>{
